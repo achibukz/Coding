a05ea635de96516f7cd6bec55c1be14949b5bdc5
Aki <99451336+achibukz@users.noreply.github.com>
2023-11-10 11:51:05 +0800

    done na

--- gexe.c
+++ gexe.c
@@ -1,66 +1,103 @@
-#include<stdio.h>
-
+#include <stdio.h>
+ 
+void checkBalance(float *fMoney){
+    printf("Balance: %.2f\n", *fMoney);
+}
+ 
+void withdraw(float *fMoney) {
+    int nAmount, nOneThousand, nOTRemain, nFiveHundred, nFHRemain, nTwoHundred, nTHRemain, nOneHundred;
+ 
+    do {
+        printf("How much money do you want to withdraw: ");
+        scanf("%d", &nAmount);
+ 
+        if (nAmount > *fMoney) {
+            printf("Withdrawal amount is greater than current money in the bank\n");
+        }
+        else if (nAmount % 100 != 0) {
+            printf("Withdrawal amount must be divisible by 100\n");
+        }
+    }
+    while (nAmount % 100 != 0 || nAmount > *fMoney);
+ 
+ 
+    *fMoney -= nAmount;
+ 
+    printf("You have withdrawn P%d\n", nAmount);
+    nOneThousand = nAmount / 1000;
+    nOTRemain = nAmount - (1000 * nOneThousand);
+    
+    nFiveHundred = nOTRemain / 500;
+    nFHRemain = nOTRemain - (500 * nFiveHundred);
+    
+    nTwoHundred = nFHRemain / 200;
+    nTHRemain = nFHRemain - (200 * nTwoHundred);
+ 
+    nOneHundred = nTHRemain / 100;
+ 
+    printf("P 1000: %d\n", nOneThousand);
+    printf("P 500: %d\n", nFiveHundred);
+    printf("P 200: %d\n", nTwoHundred);
+    printf("P 100: %d\n", nOneHundred);
+}
+ 
 void deposit(float *fMoney){
-    int nChecker1 = 1;
-    int nDepMoney, nCheck;
-
-    nCheck = nDepMoney % 100;
-
-    while(nChecker1 != 0){
-            printf("Deposited Money: \n");
-            scanf("%d", &nDepMoney);
-        if (nCheck == 0){
-            *fMoney += nDepMoney;
-            nChecker1 = 0;
-        }
-        else{
-            printf("It is an ivalid amount. Please try again.\n");
+    int nDepMoney;
+    
+    do {
+        printf("How much money do you want to deposit: ");
+        scanf("%d", &nDepMoney);
+ 
+        if (nDepMoney % 100 != 0) {
+            printf("Deposited amount must be divisible by 100\n");
         }
     }
-}
+    while (nDepMoney % 100 != 0);
 
-void DisplayMenu(){
+    *fMoney += nDepMoney;
+}
+ 
+void displayMenu(float *fMoney, int *nExit){
     int nCase;
-    int nChecker = 1;
-    float fMoney = 10000.00;
+ 
+    printf("Select Option:\n");
+    printf("1: Deposit\n");
+    printf("2: Withdraw\n");
+    printf("3: Check Balance\n");
+    printf("4: Exit\n");
+    printf("Option: ");
+    scanf("%d", &nCase);
 
-    while(nChecker != 0){
-        printf("Select Option:\n");
-        printf("1: Deposit\n");
-        printf("2: Withdraw\n");
-        printf("3: Check Balance\n");
-        printf("Option: ");
-        scanf("%d", &nCase);
-
-        switch(nCase){
-            case 1:
-                deposit(&fMoney);
-                nChecker = 0;
-                break;
-            case 2:
-                withdraw(&fMoney);
-                nChecker = 0;
-                break;
-            case 3:
-                checkBalance(&fMoney);
-                nChecker = 0;
-                break;
-            case 4:
-                printf("Shut Down.....\n");
-                nChecker = 0;
-                break;
-            default:
-                printf("Invalid Input.");
-                break;
-        }
-
+    switch(nCase){
+        case 1:
+            deposit(fMoney);
+            break;
+        case 2:
+            withdraw(fMoney);
+            break;
+        case 3:
+            checkBalance(fMoney);
+            break;
+        case 4:
+            printf("Shut Down.....\n");
+            *nExit = 1;
+            break;
+        default:
+            printf("Invalid Input.\n");
+            break;
     }
+ 
+    
 }
-
-
-
+  
 int main(){
-    DisplayMenu();
+    int nExit;
+    float fMoney;
 
+    fMoney = 10000.00;
+    nExit = 0;
+    while (nExit != 1) {
+        displayMenu(&fMoney, &nExit);
+    }
     return 0;
 }
