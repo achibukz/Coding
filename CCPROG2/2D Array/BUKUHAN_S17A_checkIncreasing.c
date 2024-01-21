74c5dd7f451a795492521931fde3167fa8ca8881
Kaixyyyy <akibukuhan10@gmail.com>
2024-01-22 01:14:33 +0800

    as

--- CCPROG2/2D Array/BUKUHAN_S17A_checkIncreasing.c
+++ CCPROG2/2D Array/BUKUHAN_S17A_checkIncreasing.c
@@ -2,30 +2,14 @@
 
 int checkIncreasing(int arr[], int elem){
     int i;
-    int temp = 0;
-    int check = 0;
+    int check = 1;
     
-    for(i = 0; i < elem - 1; i++){
-        if(i > 0){
-            arr[i - 1] = temp;
-        }
-        if(arr[i] < temp){
-            check++;
+    for(i = 1; i < elem; i++){
+        if(arr[i] <= arr[i - 1]){
+            check = 0;
+            break;
         }
     }
 
     return check;
-}
-
-
-int main(){
-    int arr[5] = {1,4,3,6,5};
-    int elem = 5;
-    int x;
-
-    x = checkIncreasing(arr, elem);
-
-    printf("%d", x);
-
-    return 0;
 }
